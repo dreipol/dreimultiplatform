baeb0e867853ed8a3954a86db258efb1bde7dc3e
Kai Widmer <>
2020-07-30 12:05:05 +0200

    Increase version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 group = "ch.dreipol"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.2-SNAPSHOT"
 
 buildscript {
     repositories {
--- src/commonMain/kotlin/ch/dreipol/dreimultiplatform/MyClass.kt
+++ src/commonMain/kotlin/ch/dreipol/dreimultiplatform/MyClass.kt
@@ -1,5 +1,9 @@
 package ch.dreipol.dreimultiplatform
 
-
+class MyClass {
+    init {
+        print("common")
+    }
+}
 
 expect fun test()
