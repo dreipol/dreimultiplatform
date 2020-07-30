6f52af6d5624899c2f946a63ae233929045b0762
Kai Widmer <>
2020-07-30 10:52:32 +0200

    Change to multiplatform project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,53 @@
 group = "ch.dreipol"
 version = "0.0.1-SNAPSHOT"
 
+buildscript {
+    repositories {
+        google()
+        jcenter()
+        maven {
+            url = uri("https://plugins.gradle.org/m2/")
+        }
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
 plugins {
     `maven-publish`
-    kotlin("jvm") version "1.3.71"
+    kotlin("multiplatform")
 }
 
-repositories {
-    jcenter()
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
 }
 
-dependencies {
-    implementation(kotlin("stdlib"))
+kotlin {
+    ios("ios")
+    jvm("android")
+    sourceSets["commonMain"].dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-common")
+    }
+    sourceSets["androidMain"].dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    }
+    sourceSets["iosMain"].dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    }
 }
 
 publishing {
     publications {
         create<MavenPublication>("default") {
-            from(components["java"])
+            from(components["kotlin"])
         }
     }
     repositories {
