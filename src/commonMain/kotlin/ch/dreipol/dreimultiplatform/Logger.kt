6710c4b2833b288beb50ef8a8409e0e4a9df277c
Kai Widmer <>
2020-08-27 11:29:16 +0200

    Remove ktor

--- src/commonMain/kotlin/ch/dreipol/dreimultiplatform/Logger.kt
+++ src/commonMain/kotlin/ch/dreipol/dreimultiplatform/Logger.kt
@@ -2,14 +2,7 @@ package ch.dreipol.dreimultiplatform
 
 import co.touchlab.kermit.CommonLogger
 import co.touchlab.kermit.Kermit
-import io.ktor.client.features.logging.*
 
 fun kermit(): Kermit {
     return Kermit(CommonLogger())
-}
-
-class HttpLogger : Logger {
-    override fun log(message: String) {
-        kermit().d { message }
-    }
 }
