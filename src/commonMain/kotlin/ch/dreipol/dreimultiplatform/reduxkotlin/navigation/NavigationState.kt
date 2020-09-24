624c5dda5b2fc82ba781ea64ce278cae42df38dd
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2020-09-24 18:58:40 +0200

    [current_screen] Add current screen helper

--- src/commonMain/kotlin/ch/dreipol/dreimultiplatform/reduxkotlin/navigation/NavigationState.kt
+++ src/commonMain/kotlin/ch/dreipol/dreimultiplatform/reduxkotlin/navigation/NavigationState.kt
@@ -1,6 +1,8 @@
 package ch.dreipol.dreimultiplatform.reduxkotlin.navigation
 
-data class NavigationState(val screens: List<Screen>, val navigationDirection: NavigationDirection)
+data class NavigationState(val screens: List<Screen>, val navigationDirection: NavigationDirection) {
+    val currentScreen = screens.last()
+}
 
 enum class NavigationDirection {
     PUSH,
