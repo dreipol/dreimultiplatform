c0649132ef63e296b215e040a1426869596951d6
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2020-10-13 15:37:12 +0200

    Fix date utils in iOS

--- src/iosMain/kotlin/ch/dreipol/dreimultiplatform/DateUtils.kt
+++ src/iosMain/kotlin/ch/dreipol/dreimultiplatform/DateUtils.kt
@@ -8,19 +8,13 @@ actual fun getLocalizedMonthName(month: Int): String {
     val dateComponents = calendar.components(NSCalendarUnitHour, fromDate = NSDate.date())
     dateComponents.setMonth(month.convert())
     val stringFormat = "MMMM"
-    val format = NSDateFormatter.alloc() ?: return ""
+    val format = NSDateFormatter()
     format.setDateFormat(stringFormat)
     val date = calendar.dateFromComponents(dateComponents) ?: return ""
     return format.stringFromDate(date)
 }
 
 actual fun getLocalizedDayShort(day: Int): String {
-    val calendar = NSCalendar.currentCalendar
-    val dateComponents = calendar.components(NSCalendarUnitHour, fromDate = NSDate.date())
-    dateComponents.setDay(day.convert())
-    val stringFormat = "DDD"
-    val format: NSDateFormatter = NSDateFormatter.alloc() ?: return ""
-    format.setDateFormat(stringFormat)
-    val date = calendar.dateFromComponents(dateComponents) ?: return ""
-    return format.stringFromDate(date)
+    val calendar = NSCalendar.autoupdatingCurrentCalendar
+    return  calendar.weekdaySymbols[day] as? String ?: ""
 }
