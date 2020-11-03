4cd84342b5e54deb463509c6e370f15cde9edcf9
Kai Widmer <>
2020-11-03 16:29:47 +0100

    Fix java weekdays index issue

--- src/androidMain/kotlin/ch/dreipol/dreimultiplatform/DateUtils.kt
+++ src/androidMain/kotlin/ch/dreipol/dreimultiplatform/DateUtils.kt
@@ -7,5 +7,12 @@ actual fun getLocalizedMonthName(month: Int): String {
 }
 
 actual fun getLocalizedDayShort(day: Int): String {
-    return DateFormatSymbols().shortWeekdays[day - 1]
+    return DateFormatSymbols().shortWeekdays[isoWeekDayToJavaIndex(day)]
+}
+
+private fun isoWeekDayToJavaIndex(isoWeekDay: Int): Int {
+    if (isoWeekDay == 7) {
+        return 1
+    }
+    return isoWeekDay + 1
 }
