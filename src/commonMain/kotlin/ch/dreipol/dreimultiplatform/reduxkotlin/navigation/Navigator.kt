b56b4430a279eb2e1bf5a5da7ab8fcc0c367b550
Kai Widmer <>
2021-01-21 09:05:18 +0100

    Make navigator state generic

--- src/commonMain/kotlin/ch/dreipol/dreimultiplatform/reduxkotlin/navigation/Navigator.kt
+++ src/commonMain/kotlin/ch/dreipol/dreimultiplatform/reduxkotlin/navigation/Navigator.kt
@@ -3,15 +3,14 @@ package ch.dreipol.dreimultiplatform.reduxkotlin.navigation
 import ch.dreipol.dreimultiplatform.reduxkotlin.selectFixed
 import org.reduxkotlin.Store
 import org.reduxkotlin.StoreSubscriber
-import org.reduxkotlin.selectors
 
-interface Navigator<RootState : Any> {
+interface Navigator<RootState : Any, NavigationState : Any> {
     val store: Store<RootState>
     fun updateNavigationState(navigationState: NavigationState)
     fun getNavigationState(): NavigationState
 }
 
-fun Navigator<*>.subscribeNavigationState(): StoreSubscriber {
+fun <RootState : Any, NavigationState : Any> Navigator<RootState, NavigationState>.subscribeNavigationState(): StoreSubscriber {
     return store.selectFixed({ getNavigationState() }) {
         updateNavigationState(getNavigationState())
     }
