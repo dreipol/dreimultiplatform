e097cc348d6160d6a410e7fe0960a8086ae62979
Nils Becker <nils.becker@dreipol.ch>
2022-10-21 10:02:45 +0200

    [file_system] Fix compilation

--- src/commonMain/kotlin/ch/dreipol/dreimultiplatform/CoroutineHelper.kt
+++ src/commonMain/kotlin/ch/dreipol/dreimultiplatform/CoroutineHelper.kt
@@ -1,14 +1,3 @@
 package ch.dreipol.dreimultiplatform
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
-
 expect fun launchAndWait(block: suspend () -> Unit)
-
-suspend fun <T> doAndAwait(`do`: suspend () -> Unit, await: suspend () -> T): T = coroutineScope {
-    awaitAll<Either<Unit, T>>(
-        async { Either.First(`do`()) },
-        async { Either.Second(await()) }
-    )[1].getSecond()
-}
---
+++ src/commonMain/kotlin/ch/dreipol/dreimultiplatform/CoroutineUtils.kt
@@ -0,0 +1,12 @@
+package ch.dreipol.dreimultiplatform
+
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+
+suspend fun <T> doAndAwait(`do`: suspend () -> Unit, await: suspend () -> T): T = coroutineScope {
+    awaitAll<Either<Unit, T>>(
+        async { Either.First(`do`()) },
+        async { Either.Second(await()) }
+    )[1].getSecond()
+}
