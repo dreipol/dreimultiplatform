04485c4dd2034863f881ba52a5cf06633d96009d
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2022-11-28 18:32:45 +0100

    [file-methods] Add more file helpers

--- src/androidMain/kotlin/ch/dreipol/dreimultiplatform/FileManager.kt
+++ src/androidMain/kotlin/ch/dreipol/dreimultiplatform/FileManager.kt
@@ -1,8 +1,16 @@
 package ch.dreipol.dreimultiplatform
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import java.io.File
+import java.io.IOException
+import java.nio.file.Files
+import kotlin.streams.toList
 
 actual typealias FileIdentifier = File
+
+actual val FileIdentifier.fileName: String?
+    get() = this.name
 
 actual val FileIdentifier.filePath: String?
     get() = this.path
@@ -10,11 +18,31 @@ actual val FileIdentifier.filePath: String?
 actual fun FileIdentifier.appendingPathComponent(component: String): FileIdentifier? =
     File(this, component)
 
+@RequiresApi(Build.VERSION_CODES.O)
+actual fun FileIdentifier.createDirectoriesIfNotExists() {
+    val path = toPath()
+    if (Files.notExists(path)) {
+        Files.createDirectories(path)
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+actual fun FileIdentifier.exists(): Boolean = Files.exists(toPath())
+
+@RequiresApi(Build.VERSION_CODES.O)
+actual fun FileIdentifier.delete() = Files.delete(toPath())
+
+@RequiresApi(Build.VERSION_CODES.O)
+actual fun FileIdentifier.files(): List<FileIdentifier> {
+    if (!Files.isDirectory(toPath())) {
+        throw IOException("The given FileIdentifier was not a directory.")
+    }
+    return Files.list(toPath()).map { it.toFile() }.toList()
+}
+
+
 actual object FileManager {
     actual fun stringFrom(file: FileIdentifier): String? = file.readText()
 
     actual fun byteArrayFrom(file: FileIdentifier): ByteArray? = file.readBytes()
 }
-
-actual val FileIdentifier.fileName: String?
-    get() = this.name
--- src/iosMain/kotlin/ch/dreipol/dreimultiplatform/FileManager.kt
+++ src/iosMain/kotlin/ch/dreipol/dreimultiplatform/FileManager.kt
@@ -1,6 +1,21 @@
 package ch.dreipol.dreimultiplatform
 
-import platform.Foundation.*
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.ObjCObjectVar
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.ptr
+import kotlinx.cinterop.value
+import platform.Foundation.NSData
+import platform.Foundation.NSError
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSString
+import platform.Foundation.NSURL
+import platform.Foundation.NSUTF8StringEncoding
+import platform.Foundation.URLByAppendingPathComponent
+import platform.Foundation.dataWithContentsOfURL
+import platform.Foundation.lastPathComponent
+import platform.Foundation.stringWithContentsOfURL
 
 actual data class FileIdentifier(val url: NSURL)
 
@@ -14,6 +29,43 @@ actual val FileIdentifier.fileName: String?
 actual val FileIdentifier.filePath: String?
     get() = this.url.path
 
+typealias ErrorPointer = CPointer<ObjCObjectVar<NSError?>>
+
+private fun <R> fileManagerWithException(block: (fileManager: NSFileManager, errorPtr: ErrorPointer) -> R): R = memScoped {
+    val errorPtr: ObjCObjectVar<NSError?> = alloc()
+    val fileManager = NSFileManager.defaultManager
+    val blockResult = block(fileManager, errorPtr.ptr)
+    val error = errorPtr.value
+    if (error != null) {
+        throw FileError(error.localizedDescription)
+    }
+    blockResult
+}
+
+actual fun FileIdentifier.createDirectoriesIfNotExists() {
+    fileManagerWithException { fileManager, errorPtr ->
+        fileManager.createDirectoryAtURL(url, withIntermediateDirectories = true, attributes = null, error = errorPtr)
+    }
+}
+
+actual fun FileIdentifier.exists(): Boolean {
+    return filePath != null && NSFileManager.defaultManager.fileExistsAtPath(filePath!!)
+}
+
+actual fun FileIdentifier.delete() {
+    fileManagerWithException { fileManager, errorPtr ->
+        fileManager.removeItemAtURL(url, error = errorPtr)
+    }
+}
+
+actual fun FileIdentifier.files(): List<FileIdentifier> = fileManagerWithException { fileManager, errorPtr ->
+    fileManager.contentsOfDirectoryAtURL(
+        url,
+        error = errorPtr,
+        includingPropertiesForKeys = null,
+        options = 0
+    )?.filterIsInstance<NSURL>() as List<NSURL>
+}.map { FileIdentifier(it) }
 
 actual object FileManager {
     actual fun stringFrom(file: FileIdentifier): String? =
