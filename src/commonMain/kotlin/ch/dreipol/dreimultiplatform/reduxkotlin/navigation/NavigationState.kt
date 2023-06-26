76b4274441190e611b277c97d3b1bf8db323e442
Julia Strasser <julia.strasser@dreipol.ch>
2023-06-26 14:17:24 +0200

    Do not deprecate navigation direction
    In Android, when navigating back from a detail page, after directly
    jumping to it e.g. through a notification deep link, the overview
    page will get animated when not knowing it is a POP-direction

--- src/commonMain/kotlin/ch/dreipol/dreimultiplatform/reduxkotlin/navigation/NavigationState.kt
+++ src/commonMain/kotlin/ch/dreipol/dreimultiplatform/reduxkotlin/navigation/NavigationState.kt
@@ -2,14 +2,14 @@ package ch.dreipol.dreimultiplatform.reduxkotlin.navigation
 
 data class NavigationState(
     val screens: List<Screen>,
-    @Deprecated("will be removed in future versions") val navigationDirection: NavigationDirection = NavigationDirection.POP,
+    val navigationDirection: NavigationDirection = NavigationDirection.POP,
     val sheet: Sheet? = null,
     val blockerScreen: Boolean = false,
 ) {
     val currentScreen = screens.last()
 }
-
-@Deprecated("will be removed in future versions")
+// used in Android for showing correct animation when
+// navigating back from deep link jump
 enum class NavigationDirection {
     PUSH,
     POP
