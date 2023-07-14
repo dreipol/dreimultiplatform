0c8f7e73c36fa108414d0676f32ac08f2e46f1b2
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2023-07-14 16:35:49 +0200

    Revert "[count_up_flow] DRY"
    This reverts commit bd6476de342380d4ecfd192b126f39f843672690.

--- src/commonMain/kotlin/ch/dreipol/dreimultiplatform/coroutines/countdownFlow.kt
+++ src/commonMain/kotlin/ch/dreipol/dreimultiplatform/coroutines/countdownFlow.kt
@@ -5,7 +5,14 @@ import kotlinx.coroutines.flow.flow
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
-fun countDownFlow(duration: Duration, period: Duration = 1.seconds) = countUpFlow(duration, period * -1)
+fun countdownFlow(duration: Duration, period: Duration = 1.seconds) = flow {
+    var current = duration
+    while (true) {
+        emit(current)
+        current -= period
+        delay(period)
+    }
+}
 
 fun countUpFlow(initial: Duration = 0.seconds, period: Duration = 1.seconds) = flow {
     var current = initial
