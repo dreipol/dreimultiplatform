dd640affa79d0f6d96e4bc5712be1e243080d513
kaiwidmer <kai.widmer@dreipol.ch>
2023-09-19 15:21:13 +0200

    [typed-store-subscriber] Add support for nullable types

--- src/commonMain/kotlin/ch/dreipol/dreimultiplatform/reduxkotlin/StoreSubscriber.kt
+++ src/commonMain/kotlin/ch/dreipol/dreimultiplatform/reduxkotlin/StoreSubscriber.kt
@@ -3,27 +3,36 @@ package ch.dreipol.dreimultiplatform.reduxkotlin
 import org.reduxkotlin.Store
 import org.reduxkotlin.StoreSubscriber
 
-fun <State : Any, T : Any> Store<State>.subscribeChanges(selection: (State) -> T, onUpdate: (T) -> Unit): StoreSubscriber {
+fun <State : Any, T : Any?> Store<State>.subscribeChanges(selection: (State) -> T, onUpdate: (T) -> Unit): StoreSubscriber {
     val selector = select(selection)
     val onStateChanged = {
-        selector.getIfChanged()?.let { onUpdate(it) }
+        selector.executeIfChanged { onUpdate(it) }
     }
     onStateChanged()
     return subscribe(onStateChanged)
 }
 
-private fun <State : Any, T : Any> Store<State>.select(selection: (State) -> T) = object : Selector<T> {
-    var lastValue: T? = null
-    override fun getIfChanged(): T? {
+private fun <State : Any, T : Any?> Store<State>.select(selection: (State) -> T) = object : Selector<T> {
+    var valueHolder: ValueHolder<T> = ValueHolder.Nothing()
+    override fun executeIfChanged(action: (T) -> Unit) {
         val newValue = selection(getState())
-        if (lastValue != newValue) {
-            lastValue = newValue
-            return newValue
+        if (valueHolder.isDifferentValue(newValue)) {
+            action(newValue)
+            valueHolder = ValueHolder.Value(newValue)
         }
-        return null
+    }
+}
+
+private sealed class ValueHolder<T : Any?> {
+    class Nothing<T : Any?> : ValueHolder<T>()
+    data class Value<T : Any?>(val value: T) : ValueHolder<T>()
+
+    fun isDifferentValue(newValue: T): Boolean = when (this) {
+        is Nothing -> true
+        is Value -> value != newValue
     }
 }
 
 private interface Selector<T> {
-    fun getIfChanged(): T?
+    fun executeIfChanged(action: (T) -> Unit)
 }
