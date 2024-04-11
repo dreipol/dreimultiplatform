0426a5065a54f967665e8ee077da600f95615f3b
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2024-04-11 08:36:49 +0200

    [localize] Add varargs to localize helper

--- src/androidMain/kotlin/ch/dreipol/dreimultiplatform/Localize.kt
+++ src/androidMain/kotlin/ch/dreipol/dreimultiplatform/Localize.kt
@@ -3,12 +3,21 @@ package ch.dreipol.dreimultiplatform
 import android.content.Context
 
 fun Context.getString(identifier: String): String {
-    val resourceId = resources.getIdentifier(identifier, "string", packageName)
+    val resourceId = stringId(identifier)
     return resources.getString(resourceId)
 }
 
+private fun Context.stringId(identifier: String): Int {
+    return resources.getIdentifier(identifier, "string", packageName)
+}
+
+fun Context.getString(identifier: String, vararg args: Any): String {
+    val stringId = stringId(identifier)
+    return resources.getString(stringId, *args)
+}
+
 class Localizer(private val context: Context) : Localize {
-    override fun localize(string: String): String {
-        return context.getString(string)
-    }
+    override fun localize(string: String): String = context.getString(string)
+
+    override fun localize(string: String, vararg args: Any): String = context.getString(string, *args)
 }
