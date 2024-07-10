b9cc273c7fca9b4cb448f6c15f3450f95240165d
Laila Becker <laila.becker@dreipol.ch>
2024-07-10 10:19:31 +0200

    [bytearray-from-string] Create ByteArray from String

--- src/commonMain/kotlin/ch/dreipol/dreimultiplatform/ByteArrayUtils.kt
+++ src/commonMain/kotlin/ch/dreipol/dreimultiplatform/ByteArrayUtils.kt
@@ -1,3 +1,17 @@
 package ch.dreipol.dreimultiplatform
 
 fun ByteArray.hex(): String = joinToString(separator = " ") { it.hex() }
+
+object ByteArrayUtils {
+    fun fromHexString(string: String): ByteArray {
+        val sanitized = string.filter { !it.isWhitespace() }
+        if (sanitized.length % 2 != 0) {
+            throw IllegalArgumentException("The input string should have an even number of characters")
+        }
+
+        return sanitized.chunked(2).map {
+            val byteValue = it.toIntOrNull(radix = 16) ?: throw IllegalArgumentException("hex string contains illegal sequence ($it)")
+            byteValue.toByte()
+        }.toByteArray()
+    }
+}
