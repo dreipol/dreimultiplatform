d4ff53fe0419475ef205c1f0afa438505bea97ef
Laila Becker <laila.becker@dreipol.ch>
2025-05-13 21:37:28 +0200

    [swift-helpers] Remove unused import

--- src/commonMain/kotlin/ch/dreipol/dreimultiplatform/PersistentKeyValueStore.kt
+++ src/commonMain/kotlin/ch/dreipol/dreimultiplatform/PersistentKeyValueStore.kt
@@ -1,7 +1,6 @@
 package ch.dreipol.dreimultiplatform
 
 import kotlinx.serialization.SerializationException
-import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
