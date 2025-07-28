c028f787cea419d5d2132e2b5af370dae111787b
Laila Becker <laila.becker@dreipol.ch>
2025-07-28 19:44:16 +0200

    [swift-helpers] Simplify gradle file setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -143,64 +143,62 @@ tasks.register<Jar>("dokkaJavadocCommonJar") {
     archiveClassifier.set("javadoc")
 }
 
-if (project == rootProject) {
-    mavenPublishing {
-        configure(KotlinMultiplatform(javadocJar = JavadocJar.Dokka("dokkaJavadocCommonJar")))
-        publishToMavenCentral()
-        signAllPublications()
-    }
+mavenPublishing {
+    configure(KotlinMultiplatform(javadocJar = JavadocJar.Dokka("dokkaJavadocCommonJar")))
+    publishToMavenCentral()
+    signAllPublications()
+}
 
-    publishing {
-        publications.withType<MavenPublication> {
-            pom {
-                name.set("dreimultiplatform")
-                description.set("Shared Repository for KMM projects")
+publishing {
+    publications.withType<MavenPublication> {
+        pom {
+            name.set("dreimultiplatform")
+            description.set("Shared Repository for KMM projects")
+            url.set("https://github.com/dreipol/dreimultiplatform")
+            scm {
                 url.set("https://github.com/dreipol/dreimultiplatform")
-                scm {
-                    url.set("https://github.com/dreipol/dreimultiplatform")
-                    connection.set("scm:https://github.com/dreipol/dreimultiplatform.git")
-                    developerConnection.set("scm:git://github.com/dreipol/dreimultiplatform.git")
+                connection.set("scm:https://github.com/dreipol/dreimultiplatform.git")
+                developerConnection.set("scm:git://github.com/dreipol/dreimultiplatform.git")
+            }
+            licenses {
+                license {
+                    name.set("The MIT License")
+                    url.set("https://opensource.org/licenses/MIT")
+                    distribution.set("repo")
                 }
-                licenses {
-                    license {
-                        name.set("The MIT License")
-                        url.set("https://opensource.org/licenses/MIT")
-                        distribution.set("repo")
-                    }
+            }
+            developers {
+                developer {
+                    id.set("melbic")
+                    name.set("Samuel Bichsel")
+                    email.set("samuel.bichsel@dreipol.ch")
                 }
-                developers {
-                    developer {
-                        id.set("melbic")
-                        name.set("Samuel Bichsel")
-                        email.set("samuel.bichsel@dreipol.ch")
-                    }
-                    developer {
-                        id.set("kaiwidmer")
-                        name.set("Kai Widmer")
-                        email.set("kai.widmer@dreipol.ch")
-                    }
-                    developer {
-                        id.set("tschuls")
-                        name.set("Julia Strasser")
-                        email.set("julia.strasser@dreipol.ch")
-                    }
-                    developer {
-                        id.set("lailabecker")
-                        name.set("Laila Becker")
-                        email.set("laila.becker@dreipol.ch")
-                    }
+                developer {
+                    id.set("kaiwidmer")
+                    name.set("Kai Widmer")
+                    email.set("kai.widmer@dreipol.ch")
+                }
+                developer {
+                    id.set("tschuls")
+                    name.set("Julia Strasser")
+                    email.set("julia.strasser@dreipol.ch")
+                }
+                developer {
+                    id.set("lailabecker")
+                    name.set("Laila Becker")
+                    email.set("laila.becker@dreipol.ch")
                 }
             }
         }
     }
+}
 
-    signing {
-        val signingKey = System.getenv("PGP_KEY")
-        var signingPassword = ""
-        if (project.hasProperty("signing.password")) {
-            signingPassword = project.property("signing.password").toString()
-        }
-        useInMemoryPgpKeys(signingKey, signingPassword)
-        sign(publishing.publications)
+signing {
+    val signingKey = System.getenv("PGP_KEY")
+    var signingPassword = ""
+    if (project.hasProperty("signing.password")) {
+        signingPassword = project.property("signing.password").toString()
     }
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications)
 }
